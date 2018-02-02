46604500f81883ddf2b8b97faddefb7693c4b958
Kevin Upton <k_upton@live.com>
2018-02-02 11:30:51 +1000

    initial commit

---
+++ app/Http/Controllers/CoinpaymentsController.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kevin
+ * Date: 2/02/2018
+ * Time: 11:28 AM
+ */
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CoinpaymentsController extends Controller
+{
+    public function purchase (Request $request) {
+
+    }
+}
